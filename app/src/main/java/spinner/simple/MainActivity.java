bdf3acdd9bc8b16a831df8dd7d08df628ab8119b
songwenchao <79556355@qq.com>
2017-05-27 18:04:54 +0800

    重构 spinner

--- app/src/main/java/spinner/simple/MainActivity.java
+++ app/src/main/java/spinner/simple/MainActivity.java
@@ -3,11 +3,7 @@ package spinner.simple;
 import android.app.Activity;
 import android.os.Bundle;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-import view.customspinner.NiceSpinner;
+import view.customspinner.MaterialSpinner;
 
 /**
  * Author Administrator
@@ -20,8 +16,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        NiceSpinner niceSpinner = (NiceSpinner) findViewById(R.id.nice_spinner);
-        List<String> dataset = new LinkedList<>(Arrays.asList("One", "Two", "Three", "Four", "Five"));
-        niceSpinner.attachDataSource(R.layout.child, dataset);
+        String[] ITEMS = {"Item 11111111111111", "Item 2", "Item 3", "Item 4"};
+        MaterialSpinner spinner = (MaterialSpinner) findViewById(R.id.spinner);
+        spinner.setData(this, R.layout.spinner_item, ITEMS);
     }
 }
